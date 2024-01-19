8fd52d6bf35e463fd153095d8d388244ca27e74b
AlejandroFelicito <alejandro.felicito@outlook.com>
2024-01-19 13:22:03 -0600

    cleaned optionChanged; draft markers and barchart

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,48 +1,86 @@
-// Create the map centered at the US L48 and zoom 5
+
+// Create the map centered at TX
 let myMap = L.map("map", {
-    center: [20.5335298, -99.9407924],
-    zoom: 5,
+  center: [20.5335298, -99.9407924],
+  zoom: 5,
 });
 
 // Insert the base layer to the map
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
 d3.json("../Datasets/project_3.museums_mx.json").then(function (data){
-    console.log(data)
-    
-    let estados = [];
-
-    estados[0] = data[0].nom_ent;
-    for (let j = 0; j < data.length; j++) {
-      if(estados.includes(data[j].nom_ent)){
-        ; 
-      } else {
-        estados.push(data[j].nom_ent);
-      }
+  let estados = [];
+  estados[0] = data[0].nom_ent;
+  for (let j = 0; j < data.length; j++) {
+    if(estados.includes(data[j].nom_ent)){
+      ; 
+    } else {
+      estados.push(data[j].nom_ent);
     }
-    console.log(estados);
-    
-
-let dropdownMenu = d3.select("#selDataset");
-    ///console.log(data[3].nom_ent)
-    estados.forEach(function(nombre){
-        ///console.log(data.nom_ent)
-        dropdownMenu.append("option").text(nombre).property("value", nombre);
-    });
-    
-    function optionChanged(selectedValue){
-    console.log(selectedValue);
-    
-    
-    //    for(let i=0; i<estados.length; i++){
-    //        let 
-    //    }
-    }
-    optionChanged();
+  };
+  
+  let dropdownMenu = d3.select("#selDataset");
+  estados.forEach(function(nombre){
+    dropdownMenu.append("option").text(nombre).property("value", nombre);
+  });
 });
 
+
+
+function plots(name_value){
+  d3.json("../Datasets/project_3.museums_mx.json").then(function(records){
+    let name_a = records.filter(rec => rec.nom_ent === name_value);
+    /* let hover = name_a[0].museo_nombre; */    
+
+    let temas = {}; 
+    for(r of name_a){
+      if(temas[r.museo_tematica_n1]){
+        temas[r.museo_tematica_n1] += 1;
+      } else {
+        temas[r.museo_tematica_n1] = 1;
+      }
+    }; 
+    
+    let bar_y = Object.keys(temas);
+    let bar_x = Object.values(temas);
+    console.log(bar_x, bar_y);
+    let bar_a = [{
+      type: "bar",
+      x: bar_x,
+      y: bar_y,
+      /* text: ,  */
+      orientation: "h",
+    }]; 
+
+    Plotly.newPlot("bar", bar_a);  
+
+    let markers = [];
+    for(let i = 0; i < name_a.length; i++) {
+      /* let museo = name_a[i]; */
+      let marker = L.marker([name_a[i].gmaps_latitud, name_a[i].gmaps_longitud])
+        .bindPopup(`<h1>${name_a[i].museo_nombre}</h1> <hr> 
+        <h3>Municipio: ${name_a[i].nom_mun}</h3> <hr> 
+        <h3>Localidad: ${name_a[i].nom_loc}</h3>`)
+        .addTo(myMap);
+      markers.push(marker);
+    }
+  
+  });
+};
+
+
+
+
+
+
+
+
+function optionChanged(selectedValue){
+  /* console.log(selectedValue); */
+  plots(selectedValue);
+}
 
 
 //create a function to set the marker
@@ -56,21 +94,6 @@ for (let i = 0; i < 30; i++) {
       .bindPopup(`<h1>${museo.museo_nombre}</h1> <hr> <h3>Population ${museo.nom_ent.toLocaleString()}</h3>`)
       .addTo(myMap);
     markers.push(marker)
-  }
-
-  console.log(markers)
-
-
-
-
-/*     let dropdownMenu = d3.select("#selDataset");
-    console.log(data[3].nom_ent)
-    data.forEach(function(nom_ent){
-        console.log(data.nom_ent)
-        dropdownMenu.append("option").text(nom_ent).property("value", nom_ent);
-    }); */
-
-    //function findUniqueValues(markers) { return markers.filter((nom_ent, museo, self) => { return self.indexOf(nom_ent) === museo; }); }
-
+  } */
 
 
